8d3c5dbdca61f3f50cb03e6c60309ea9dfcff4ee
Vitordan5 <55815066+Vitordan5@users.noreply.github.com>
2021-11-06 19:42:36 -0300

    Atualização DW
    atualização DW

--- dw/scrips_table_dw.sql
+++ dw/scrips_table_dw.sql
@@ -1,140 +1,204 @@
--- -----------------------------------------------------
--- Table dim_usuario
--- -----------------------------------------------------
-CREATE TABLE dim_usuario (
-  id_usuario BIGINT IDENTITY NOT NULL PRIMARY KEY NONCLUSTERED,
-  nome_usuario VARCHAR(100) NOT NULL,
-  email VARCHAR(100) NOT NULL,
-  codigo_permissao INT NOT NULL,
-  descricao_permissao VARCHAR(50) NOT NULL,
-  tel_1 VARCHAR(45) NULL,
-  tel_2 VARCHAR(45) NULL
- );
+CREATE TABLE IF NOT EXISTS public.dim_aula
+(
+    id_aula bigserial NOT NULL,
+    data_inicio timestamp without time zone NOT NULL,
+    data_fim timestamp without time zone NOT NULL,
+    id_disciplina bigint NOT NULL,
+    titulo character varying(45),
+    duracao double precision NOT NULL,
+    assunto character varying(100),
+    PRIMARY KEY (id_aula)
+);
 
--- -----------------------------------------------------
--- Table dim_turma
--- -----------------------------------------------------
-CREATE TABLE dim_turma (
-  id_turma BIGINT IDENTITY NOT NULL PRIMARY KEY NONCLUSTERED,
-  descricao VARCHAR(50) NOT NULL,
-  id_professor BIGINT NOT NULL,
-  INDEX fk_id_professor_idx (id_professor ASC),
-  CONSTRAINT fk_id_professor
-    FOREIGN KEY (id_professor)
-    REFERENCES dim_usuario (id_usuario)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-	GO
+CREATE TABLE IF NOT EXISTS public.dim_curso
+(
+    id_curso serial NOT NULL,
+    descricao character varying(50) NOT NULL,
+    graduacao character varying(50) NOT NULL,
+    duracao integer NOT NULL,
+    PRIMARY KEY (id_curso)
+);
 
--- -----------------------------------------------------
--- Table dim_curso
--- -----------------------------------------------------
-CREATE TABLE dim_curso (
-  id_curso INT IDENTITY NOT NULL PRIMARY KEY NONCLUSTERED,
-  descricao VARCHAR(50) NOT NULL,
-  graduação VARCHAR(50) NOT NULL,
-  duracao INT NOT NULL,
-  )
-GO
+CREATE TABLE IF NOT EXISTS public.dim_disciplina
+(
+    id_disciplina bigserial NOT NULL,
+    id_curso integer NOT NULL,
+    descricao character varying(50) NOT NULL,
+    id_turma bigint NOT NULL,
+    PRIMARY KEY (id_disciplina)
+);
 
--- -----------------------------------------------------
--- Table dim_disciplina
--- -----------------------------------------------------
-CREATE TABLE dim_disciplina (
-  id_disciplina BIGINT IDENTITY NOT NULL PRIMARY KEY NONCLUSTERED,
-  id_curso INT NOT NULL,
-  descricao VARCHAR(50) NOT NULL,
-  id_turma BIGINT NOT NULL,
-  INDEX fk_id_curso_idx (id_curso ASC),
-  CONSTRAINT fk_id_curso
-    FOREIGN KEY (id_curso)
-    REFERENCES dim_curso (id_curso)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-GO
+CREATE TABLE IF NOT EXISTS public.dim_turma
+(
+    id_turma bigserial NOT NULL,
+    descricao character varying(50) NOT NULL,
+    id_professor bigint NOT NULL,
+    PRIMARY KEY (id_turma)
+);
+
+CREATE TABLE IF NOT EXISTS public.dim_usuario
+(
+    id_usuario bigserial NOT NULL,
+    nome_usuario character varying(100),
+    username character varying(50),
+    codigo_perfil integer NOT NULL,
+    descricao_perfil character varying(50) NOT NULL,
+    UNIQUE (username),
+    PRIMARY KEY (id_usuario)
+);
+
+CREATE TABLE IF NOT EXISTS public.fact_aluno_aula
+(
+    id_aluno_aula bigserial NOT NULL,
+    id_aluno bigint NOT NULL,
+    id_aula bigint NOT NULL,
+    tempo_participacao double precision,
+    reacao double precision,
+    PRIMARY KEY (id_aluno_aula)
+);
+
+CREATE TABLE IF NOT EXISTS public.fact_acesso
+(
+    id_usuario bigserial NOT NULL,
+    data_login timestamp without time zone NOT NULL,
+    data_logoff timestamp without time zone NOT NULL,
+    origem character (1),
+    PRIMARY KEY(id_usuario,data_login,origem)
+);
+
+CREATE TABLE IF NOT EXISTS public.fact_matricula
+(
+    id_matricula bigserial NOT NULL,
+    data_inicio timestamp without time zone NOT NULL,
+    data_fim timestamp without time zone NOT NULL,
+    id_aluno bigint NOT NULL,
+    id_turma bigint NOT NULL,
+    id_disciplina bigint NOT NULL,
+    id_certificado bigint,
+    numero_certificado character varying(45),
+    status_matricula bit(1) NOT NULL,
+    quantidade_matricula integer NOT NULL,
+    PRIMARY KEY (id_matricula)
+);
+
+CREATE TABLE IF NOT EXISTS public.fact_nota
+(
+    id_nota bigserial NOT NULL,
+    id_aluno bigint NOT NULL,
+    id_disciplina bigint NOT NULL,
+    nota double precision NOT NULL,
+    nota_avaliacao double precision NOT NULL,
+    percentual_nota double precision NOT NULL,
+    data timestamp without time zone NOT NULL,
+    PRIMARY KEY (id_nota)
+);
+
+CREATE TABLE IF NOT EXISTS public.dim_chat
+(
+    id_chat character varying(50) NOT NULL,
+    data_inicio timestamp without time zone NOT NULL,
+    data_fim timestamp without time zone NOT NULL,
+    quantidade_usuario int NOT NULL,
+    descricao character varying(80) NOT NULL,
+    duracao double precision NOT NULL,
+    PRIMARY KEY (id_chat)
+);
+
+CREATE TABLE IF NOT EXISTS public.fact_usuario_chat
+(
+   id_usuario_chat bigserial NOT NULL,
+   id_usuario bigint NOT NULL,
+   id_chat character varying(50) NOT NULL,
+   data_login timestamp without time zone NOT NULL,
+   data_logoff timestamp without time zone NOT NULL,
+   quantidade_mensagens integer NOT NULL,
+   data_ultima_msg timestamp without time zone NOT NULL,
+   tempo_participacao double precision NOT NULL,
+   PRIMARY KEY (id_usuario_chat)
+);
+
+CREATE TABLE IF NOT EXISTS public.fact_usuario_conteudo
+(
+	id_usuario_conteudo bigserial NOT NULL,
+    id_usuario bigint NOT NULL,
+	data_upload timestamp without time zone NOT NULL,
+	data_download timestamp without time zone NOT NULL,
+    id_conteudo bigint NOT NULL,
+    id_disciplina bigint NOT NULL,
+    id_aula bigint NOT NULL,
+	PRIMARY KEY (id_usuario_conteudo)	
+);
 
 
--- -----------------------------------------------------
--- Table fact_matricula
--- -----------------------------------------------------
-CREATE TABLE fact_matricula (
-  id_matricula BIGINT PRIMARY KEY NOT NULL,
-  data_inicio DATETIME NOT NULL,
-  data_fim DATETIME NOT NULL,
-  id_aluno BIGINT NOT NULL REFERENCES dim_usuario (id_usuario),
-  id_turma BIGINT NOT NULL REFERENCES dim_turma (id_turma),
-  id_disciplina BIGINT NOT NULL REFERENCES dim_disciplina (id_disciplina),
-  id_certificado BIGINT NULL,
-  numero_certificado VARCHAR(45) NULL,
-  status_matricula BIT NOT NULL,
-  quantidade_matricula INT NOT NULL,
-  INDEX fk_data_inicio_idx (data_inicio ASC),
-  INDEX fk_data_fim_idx (data_fim ASC)
-  )
-  GO
 
--- -----------------------------------------------------
--- Table fact_nota
--- -----------------------------------------------------
-CREATE TABLE fact_nota (
-  id_nota BIGINT PRIMARY KEY NOT NULL,
-  id_aluno BIGINT NOT NULL REFERENCES dim_usuario (id_usuario),
-  nota FLOAT NOT NULL,
-  nota_avaliacao FLOAT NOT NULL,
-  percentual_nota FLOAT NOT NULL,
-  id_disciplina BIGINT NOT NULL REFERENCES dim_disciplina (id_disciplina),
-  data DATETIME NOT NULL,
-  INDEX fk_id_aluno_idx (id_aluno ASC),
-  INDEX fk_id_disciplina_idx (id_disciplina ASC)
-  )
-  GO
+ALTER TABLE public.dim_disciplina
+    ADD FOREIGN KEY (id_curso)
+    REFERENCES public.dim_curso (id_curso)
+    NOT VALID;
 
+ALTER TABLE public.dim_aula
+    ADD FOREIGN KEY (id_disciplina)
+    REFERENCES public.dim_disciplina (id_disciplina)
+    NOT VALID;
 
--- -----------------------------------------------------
--- Table dim_aula
--- -----------------------------------------------------
-CREATE TABLE dim_aula (
-  id_aula BIGINT PRIMARY KEY NOT NULL,
-  data DATETIME NOT NULL,
-  id_disciplina BIGINT NOT NULL REFERENCES dim_disciplina (id_disciplina),
-  titulo VARCHAR(45) NULL,
-  duracao FLOAT NOT NULL,
-  assunto VARCHAR(100) NULL,
-  INDEX fk_id_disciplina_idx (id_disciplina ASC),
-  INDEX fk_data_idx (data ASC) 
-  )
-GO
+ALTER TABLE public.fact_aluno_aula
+    ADD FOREIGN KEY (id_aluno)
+    REFERENCES public.dim_usuario (id_usuario)
+    NOT VALID;
 
+ALTER TABLE public.fact_aluno_aula
+    ADD FOREIGN KEY (id_aula)
+    REFERENCES public.dim_aula (id_aula)
+    NOT VALID;
 
--- -----------------------------------------------------
--- Table fact_aluno_aula
--- -----------------------------------------------------
-CREATE TABLE fact_aluno_aula (
-  id_aluno_aula BIGINT PRIMARY KEY NOT NULL,
-  id_aluno BIGINT NOT NULL REFERENCES dim_usuario (id_usuario),
-  reacao_valor FLOAT NULL,
-  data_reacao DATETIME NOT NULL, 
-  id_aula BIGINT NOT NULL REFERENCES dim_aula (id_aula),
-  quantidade_msg INT NULL,
-  tempo_participacao FLOAT NULL,
-  media_reacao FLOAT NULL,
+ALTER TABLE public.fact_acesso
+    ADD FOREIGN KEY (id_usuario)
+    REFERENCES public.dim_usuario (id_usuario)
+    NOT VALID;
 
-  INDEX fk_data_idx (data_reacao ASC),
-  INDEX fk_id_aula_idx (id_aula ASC),
-  INDEX fk_id_aluno_idx (id_aluno ASC)
-  )
-  GO
+ALTER TABLE public.fact_matricula
+    ADD FOREIGN KEY (id_aluno)
+    REFERENCES public.dim_usuario (id_usuario)
+    NOT VALID;
 
+ALTER TABLE public.fact_matricula
+    ADD FOREIGN KEY (id_disciplina)
+    REFERENCES public.dim_disciplina (id_disciplina)
+    NOT VALID;
 
--- -----------------------------------------------------
--- Table fact_login_plataforma
--- -----------------------------------------------------
-CREATE TABLE fact_login_plataforma (
-  id_usuario BIGINT NOT NULL PRIMARY KEY REFERENCES dim_usuario (id_usuario),
-  data_login DATETIME NOT NULL,
-  data_logoff  DATETIME NOT NULL,
-  quantidade_acesso INT NOT NULL,
-  INDEX fk_data_login_idx (data_login ASC),
-  INDEX fk_data_logoff_idx (data_logoff ASC)
-	)
-GO
+ALTER TABLE public.fact_matricula
+    ADD FOREIGN KEY (id_turma)
+    REFERENCES public.dim_turma (id_turma)
+    NOT VALID;
+
+ALTER TABLE public.fact_nota
+    ADD FOREIGN KEY (id_aluno)
+    REFERENCES public.dim_usuario (id_usuario)
+    NOT VALID;
+
+ALTER TABLE public.fact_nota
+    ADD FOREIGN KEY (id_disciplina)
+    REFERENCES public.dim_disciplina (id_disciplina)
+    NOT VALID;
+
+ALTER TABLE public.fact_usuario_conteudo
+    ADD FOREIGN KEY (id_usuario)
+    REFERENCES public.dim_usuario (id_usuario)
+    NOT VALID;
+
+ALTER TABLE public.fact_usuario_conteudo
+    ADD FOREIGN KEY (id_disciplina)
+    REFERENCES public.dim_disciplina (id_disciplina)
+    NOT VALID;
+
+ALTER TABLE public.fact_usuario_conteudo
+    ADD FOREIGN KEY (id_aula)
+    REFERENCES public.dim_aula (id_aula)
+    NOT VALID;
+
+ALTER TABLE public.fact_usuario_chat
+    ADD FOREIGN KEY (id_usuario)
+    REFERENCES public.dim_usuario (id_usuario)
+    NOT VALID;
+
+END;
